c05e56cd726483ef3a8025d4a763ae47deeacf4e
Lou Dace <lsdace30095@users.noreply.github.com>
2023-02-21 22:11:06 -0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -160,3 +160,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://smartcity23.azurewebsites.net",
+    "https://smartcity23.sensetrafficpulse.com",
+    "https://smartcity23.traffictv.info",
+    "https://smartcity23.dace-it.us",
+]
